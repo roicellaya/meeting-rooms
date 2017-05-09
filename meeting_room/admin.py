68cce3c66b1747359fc5fac794e3d6adb7ec1b9a
Roicel Laya <roicel.laya@gmail.com>
2017-05-09 00:13:14 -0300

    Agregados modelos para ser modificados por el usuario admin.

--- meeting_room/admin.py
+++ meeting_room/admin.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
-
 # Register your models here.
+from models import Equipment, MeetingRoom
+
+admin.site.register(Equipment)
+admin.site.register(MeetingRoom)
